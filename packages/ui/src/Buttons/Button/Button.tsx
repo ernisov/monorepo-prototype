2617b3a7a562e0a1954793c82f68faa06a5e581e
sercdmtn <87174957+sercdmtn@users.noreply.github.com>
2022-06-26 01:10:56 +0800

    add styles support to ui package

--- packages/ui/src/Buttons/Button/Button.tsx
+++ packages/ui/src/Buttons/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './Button.module.css';
 
 export type ButtonProps = {
     children: React.ReactNode;
 };
 
 const Button = ({ children }: ButtonProps) => {
-    return <button>{children}</button>;
+    return <button className={styles.button}>{children}</button>;
 };
 
 export default Button;
