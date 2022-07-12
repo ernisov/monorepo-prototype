09ad2405370c1d5e0e951e58d315422b1e97a079
Sultan Ernisov <ernisov@gmail.com>
2022-07-13 02:49:42 +0800

    build (ui): Add cjs module build

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -13,11 +13,18 @@ const packageJson = require('./package.json');
 export default [
     {
         input: 'src/index.ts',
-        output: {
-            file: packageJson.module,
-            format: 'esm',
-            sourcemap: true,
-        },
+        output: [
+            {
+                file: packageJson.module,
+                format: 'esm',
+                sourcemap: true,
+            },
+            {
+                file: packageJson.main,
+                format: 'cjs',
+                sourcemap: true,
+            }
+        ],
         plugins: [
             peerDepsExternal(),
             del({ targets: 'dist/*' }),
