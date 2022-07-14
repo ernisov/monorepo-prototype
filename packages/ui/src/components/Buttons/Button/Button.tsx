f3181d8442cc93576019b79ac74063400fde1813
Sultan Ernisov <ernisov@gmail.com>
2022-07-14 21:33:08 +0800

    feat (ui): Add click prop to Button

--- packages/ui/src/components/Buttons/Button/Button.tsx
+++ packages/ui/src/components/Buttons/Button/Button.tsx
@@ -3,10 +3,11 @@ import styles from './Button.module.css';
 
 export type ButtonProps = {
     children: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 
-const Button = ({ children }: ButtonProps) => {
-    return <button className={styles.button}>{children}</button>;
+const Button = ({ children, onClick }: ButtonProps) => {
+    return <button className={styles.button} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
